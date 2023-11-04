fe44e6f37c5ba25abcfeceff3621dbaae8faeaa1
qudy17 <118619952+qudy17@users.noreply.github.com>
2023-11-04 20:26:18 +0500

    Add files via upload
    asdas

---
+++ project/index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+
+	<!-- Slick slider -->
+	<link rel="stylesheet" href="./libs/slick/slick.css">
+	<link rel="stylesheet" href="./libs/slick/slick-theme.css">
+
+<!-- 	Наш style.css -->
+<link rel="stylesheet" href="style.css">
+</head>
+
+<header class="header">
+	<a href="">
+		<p class="header__logo">plot//
+			\\pizza</p>
+	</a>
+	<div class="burger-menu">
+		<span></span>
+	</div>
+	<nav class="main-nav">
+		<ul class="nav-list">
+			<li class="list__item"><a href="#on">О нас</a></li>
+			<li class="list__item"><a href="#car">Каталог</a></li>
+			<li class="list__item"><a href="#op">Описание</a></li>
+			<li class="list__item"><a href="#php">Форма для запаза</a></li>
+			<li class="list__item"><a href="">Заказы</a></li>
+		</ul>
+	</nav>
+</header>
+
+<body>
+
+
+
+	<section class="section1">
+        <img class="logo_photo" src="photo/99.png">
+		<div class="texts1" id="on">
+			<h1 >Плотная пицца для Плотных посиделок</h1>
+			<p>Плотность наших пиц измеряется в ваших эмоциях</p>
+		</div> 
+    </section>
+	
+	<div id="pusto"></div>
+
+	<div class="carousel" id="car">
+
+		<div class="slide">
+
+			<img class="photo" src="photo/peperoni1.png" alt="slide1" >
+
+			<p class="text">Пепперони </p>
+
+
+		</div>
+
+		<div class="slide">
+
+			<img class="photo" src="photo/vs1.png" alt="slide1">
+
+			<p class="text">Ветчина и сыр</p>
+
+		</div>
+
+		<div class="slide">
+
+			<img class="photo" src="photo/cheese1.png" alt="slide1">
+
+			<p class="text">Сырная</p>
+
+
+		</div>
+
+		<div class="slide">
+
+			<img class="photo" src="photo/cb1.png" alt="slide1">
+
+			<p class="text">Цыпленок барбекю</p>
+
+
+		</div>
+
+		<div class="slide">
+
+			<img class="photo" src="photo/4ch1.png" alt="slide1">
+
+			<p class="text">Четыре сыра</p>
+
+
+		</div>
+
+		<div class="slide">
+
+			<img class="photo" src="photo/ck1.png" alt="slide1">
+
+			<p class="text">Цыпленок карри</p>
+
+
+		</div>
+
+		<div class="slide">
+
+			<img class="photo" src="photo/cr1.png" alt="slide1">
+
+			<p class="text">Цыпленок ранч</p>
+
+
+		</div>
+
+		<div class="slide">
+
+			<img class="photo" src="photo/vs1.png" alt="slide1">
+
+			<p class="text">Ветчина и грибы</p>
+
+
+		</div>
+
+		<div class="slide">
+
+			<img class="photo" src="photo/gav1.png" alt="slide1">
+
+			<p class="text">Гавайская</p>
+
+
+		</div>
+
+	</div>
+
+	<div id="pusto"></div>
+
+	<section class="section1" id="op">
+		<img class="photo2" src="photo/peperoni.png" alt="pizza">
+		<div class="texts1">
+			<h1>Пепперони</h1>
+			<h2 class="price">999₽</h2>
+			<p>Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус</p>
+		</div>
+	</section>
+	<section class="section1">
+		<img class="photo2" src="photo/vs.png" alt="pizza">
+		<div class="texts1">
+			<h1>Ветчина и сыр</h1>
+			<h2 class="price">999₽</h2>
+			<p>Ветчина, моцарелла, фирменный соус альфредо</p>
+		</div>
+	</section>
+	<section class="section1">
+		<img class="photo2" src="photo/cheese.png" alt="pizza">
+		<div class="texts1">
+			<h1>Сырная</h1>
+			<h2 class="price">999₽</h2>
+			<p>Моцарелла, сыры чеддер и пармезан, фирменный соус альфредо</p>
+		</div>
+	</section>
+
+	<section class="section1">
+		<img class="photo2" src="photo/cb.png" alt="pizza">
+		<div class="texts1">
+			<h1>Цыпленок барбекю</h1>
+			<h2 class="price">999₽</h2>
+			<p>Цыпленок, бекон, соус барбекю, красный лук, моцарелла, фирменный томатный соус</p>
+		</div>
+	</section>
+
+	<section class="section1">
+		<img class="photo2" src="photo/4ch.png" alt="pizza">
+		<div class="texts1">
+			<h1>Четыре сыра</h1>
+			<h2 class="price">999₽</h2>
+			<p>Сыр блю чиз, смесь сыров чеддер и пармезан, моцарелла, фирменный соус альфредо</p>
+		</div>
+	</section>
+
+	<section class="section1">
+		<img class="photo2" src="photo/ck.png" alt="pizza">
+		<div class="texts1">
+			<h1>Цыпленок карри</h1>
+			<h2 class="price">999₽</h2>
+			<p>Цыпленок, ананасы, соус карри, красный лук, сладкий перец, моцарелла, фирменный томатный соус</p>
+		</div>
+	</section>
+
+	<section class="section1">
+		<img class="photo2" src="photo/cr.png" alt="pizza">
+		<div class="texts1">
+			<h1>Цыпленок ранч</h1>
+			<h2 class="price">999₽</h2>
+			<p>Цыпленок, ветчина, соус ранч, моцарелла, томаты, чеснок</p>
+		</div>
+	</section>
+
+	<section class="section1">
+		<img class="photo2" src="photo/vs.png" alt="pizza">
+		<div class="texts1">
+			<h1>Ветчина и грибы</h1>
+			<h2 class="price">999₽</h2>
+			<p>Ветчина, шампиньоны, увеличенная порция моцареллы, фирменный томатный соус</p>
+		</div>
+	</section>
+
+	<section class="section1">
+		<img class="photo2" src="photo/gav.png" alt="pizza">
+		<div class="texts1">
+			<h1>Гавайская</h1>
+			<h2 class="price">999₽</h2>
+			<p>Ветчина, ананасы, моцарелла, фирменный томатный соус</p>
+		</div>
+	</section>
+
+	<body >
+    <div class="wrapper">
+        <!--Поле для информации пользователя-->
+        <h2 id="php" class="wrapper__title">Заполните форму для заказа!</h2>
+        <!--Добавьте action-->
+        
+        <form class="form" action="form.php" method="post">
+            
+            
+            
+            <div class="user">
+                <ul class="user-info">
+                    <li class="user-info__item">
+                        <!--Вставь сюда label для ФИО-->
+                        <label for="name">Ф.И.О</label>
+                        <input type="text" name="name" id="name" placeholder="Введите ваше ФИО" required>
+                    </li>
+                    <li class="user-info__item">
+                        <!--Вставь сюда label для почты-->
+                        <label for="text">Название пиццы</label>
+                        <input type="email" id="email" name="email" placeholder="Введите название пиццы">
+                    </li>
+                </ul>
+                <!--Поля для адреса и даты-->
+                <ul class="address-date">
+                    <li class="user-info__item">
+                        <!--Вставь сюда label для адреса-->
+                        <label for="address">Укажите ваш адрес</label>
+                        <input type="text" name="address" id="address" placeholder="Ваш адрес">
+                    </li>
+                    <li class="user-info__item">
+                        <!--Вставь сюда label для даты заказа-->
+                        <label for="text">Укажите дату доставки</label>
+                        <input type="date" name="" id="date">
+                    </li>
+                </ul>
+            </div>
+            <div class="delivery">
+                <ul class="delivery-info">
+                    <h3 class="delivery__title">Выберете способ доставки</h3>
+                    <li class="delivery-info__item">
+                        <!--Вставь сюда label для способа доставки-->
+                        <label for="courier">Курьер</label>
+                        <input type="radio" name="delivery-type" id="courier">
+                    </li> 
+                    <li class="delivery-info__item">
+                        <!--Вставь сюда label для способа доставки-->
+                        <label for="sam">Самовывоз</label>
+                        <input type="radio" name="delivery-type" id="sam">
+                    </li>
+                </ul>
+                <ul class="delivery-info">
+                    <h3 class="delivery__title">Выберете доп.услуги</h3>
+                    <li class="delivery-info__item">
+                        <!--Вставь сюда label для доп услуг-->
+                        <label for="dop1">Быстрый курьер</label>
+                        <input type="checkbox" name="dop1" id="dop1">
+                    </li>
+                    <li class="delivery-info__item">
+                        <!--Вставь сюда label для доп услуг-->
+                        <label for="dop2">Грузчики</label>
+                        <input type="checkbox" name="dop1" id="dop2">
+                    </li>
+                    <li class="delivery-info__item">
+                        <!--Вставь сюда label для доп услуг-->
+                        <label for="dop3">Бронебойная упаковка</label>
+                        <input type="checkbox" name="dop1" id="dop3">
+                    </li>
+                </ul>
+            </div>
+            <div class="select">
+                    <h3 class="select__title">Оставьте свой комментарий и выберите оплату </h3>
+                    <select name="" id="">
+                        <option>Онлайн</option>
+                        <option>Наличными при получении</option>
+                        <option>Картой при получении</option>
+                    </select>
+                    <!--Вставь label для комментария-->
+                    <label for="comment">Дополнительный комментарий</label>
+                    <textarea name="" id="comment" cols="30" rows="10" placeholder="Ваш комментарий"></textarea>
+                </div>
+                
+                
+            <!--Вставьте кнопку-->
+            
+            
+            <input type="submit" value="Отправить данные">
+            
+            
+            
+        </form>
+    </div>
+    <script src="js/script.js"></script>
+    </body>
+
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script> 
+
+	<script src="./libs/slick/slick.min.js"></script>
+
+	<script src="./main.js"></script>
+</body>
+
+<footer id="info">Доставка осуществляется только по Перми, Екатеринбургу, Нижнему Тагилу и Челябинску. Все права защищены ©PlotPizza 2023
+</footer>
+
+</html>
